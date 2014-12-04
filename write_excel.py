b19ae4e62c450645cff4ad78c1aa8b67112fbf24
spottedzebra <mwhit74@gmail.com>
2014-12-04 17:11:03 -0600

    Differences in Binary Files?

--- write_excel.py
+++ write_excel.py
@@ -4,6 +4,12 @@ def set_by_row(sheet, data, start_row = 0, start_col = 0):
 		row = sheet.row(x + start_row)
 		for y in xrange(len(data[x])):
 			row.write(y + start_col, data[x][y])
+		
+	return x, y
+			
+def set_row(sheet, line, row = 0, start_col = 0):
+	for x in xrange(len(line)):
+		sheet.write(row, start_col + x, line[x])
 				
 def set_by_col(sheet, data, col):
 	for x in xrange(len(data)):
