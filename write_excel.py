bf7a04f43f04327f0ab84625ef0f6b35aef601f2
spottedzebra <mwhit74@gmail.com>
2015-07-01 18:00:23 -0500

    Add formatting option
    Need to be able to format for specific task and made in generic in the
    functions

--- write_excel.py
+++ write_excel.py
@@ -1,19 +1,28 @@
 
-def set_by_row(sheet, data, start_row = 0, start_col = 0):
+from xlwt import *
+
+#formatting excel
+#http://www.youlikeprogramming.com/2011/04/examples-generating-excel-documents-using-pythons-xlwt/
+#91px = 3333 units = 1 inch
+
+def set_by_row(sheet, data, start_row = 0, start_col = 0, s_type=XFStyle()):
 	for x in xrange(len(data)):
 		row = sheet.row(x + start_row)
 		for y in xrange(len(data[x])):
-			row.write(y + start_col, data[x][y])
+			row.write(y + start_col, data[x][y], style=s_type)
 		
 	return x, y
 			
-def set_row(sheet, line, row = 0, start_col = 0):
+def set_row(sheet, line, row = 0, start_col = 0, s_type=XFStyle()):
 	for x in xrange(len(line)):
-		sheet.write(row, start_col + x, line[x])
+		sheet.write(row, start_col + x, line[x], style=s_type)
 				
-def set_by_col(sheet, data, col):
+def set_by_col(sheet, data, col = 0, s_type=XFStyle()):
 	for x in xrange(len(data)):
-		sheet.write(x, col, data[x])
+		sheet.write(x, col, data[x], style=s_type)
+		
+def set_individual_cell(sheet, val, row = 0, col = 0, s_type=XFStyle()):
+	sheet.write(row, col, label=val, style=s_type)
 		
 def save(book, name):
 	book.save(name)
