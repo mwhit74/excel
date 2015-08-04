9220aa94c55bc748530a72f16b778af4c46bd1e9
spottedzebra <mwhit74@gmail.com>
2015-08-04 10:08:18 -0500

    Off-by-one error, print area optional, manager function
    Corrected the off-by-one error so the user can input the worksheet
    number starting with 1 and the program will correct for it.
    
    Originally, defining the print area was not a option, it was required.
    It is not an optional definition, if it is not defined the print area of
    each worksheet will be used as is. This will need additional work to
    specify the paper size.
    
    Defined a manager function for the user to more easily pass arguments to
    and run the program. This will need to be refactored; in it's current
    state is uses the print_multiple_ws function and does not give the user
    a choice to only print one worksheet.

--- excel_to_pdf.py
+++ excel_to_pdf.py
@@ -7,7 +7,7 @@ def open(wb_path):
 	o = win32com.client.Dispatch("Excel.Application")
 	o.Visible = False
 	wb = o.Workbooks.Open(wb_path)
-	return o, wb
+	return wb
 	
 def print_setup(ws, print_area):
 	ws.PageSetup.Zoom = False
@@ -15,37 +15,49 @@ def print_setup(ws, print_area):
 	ws.PageSetup.FitToPagesWide = 1
 	ws.PageSetup.PrintArea = print_area
 	
-def print_single_ws(wb, wb_path, ws_index, print_area, pdf_path = None):
+def print_single_ws(wb, wb_path, ws_index, print_area = None, pdf_path = None):
 	if pdf_path is None:
 		pdf_path = os.path.dirname(wb_path) + "\\" + os.path.splitext(os.path.basename(wb_path))[0]
 		
+	if print_area is not None:
+		print_setup(ws, print_area)
+		
 	ws = wb.Worksheets[ws_index]
-	print_setup(ws, print_area)
 	ws.ExportAsFixedFormat(0, pdf_path)
 	
-def print_multiple_ws(wb, wb_path, ws_index_list, print_area, pdf_path = None):
+def print_multiple_ws(wb, wb_path, ws_index_list, print_area = None, pdf_path = None):
 	if pdf_path is None:
 		pdf_path = os.path.dirname(wb_path) + "\\" + os.path.splitext(os.path.basename(wb_path))[0]
-
-	for index in ws_index_list:
-		ws = wb.Worksheets[index]
-		print_setup(ws, print_area)
+		
+	if print_area is not None:
+		for index in ws_index_list:
+			ws = wb.Worksheets[index-1]
+			print_setup(ws, print_area)
 	
 	wb.WorkSheets(ws_index_list).Select()
 	wb.ActiveSheet.ExportAsFixedFormat(0, pdf_path)
+	wb.WorkSheets(1).Select()
 	
-def close(o, wb):
+def close(wb):
 	wb.Close(True)
-	o.Quit()
+	
+def manager(wb_path, print_area, ws_index_list):
+
+	wb = open(wb_path)
+	try:
+		print_multiple_ws(wb, wb_path, ws_index_list, print_area)
+	except Exception as e:
+		print e
+	close(wb)
 
 if __name__ == "__main__":
-	wb_path = r'\\kcow00\Jobs3\50790\Bridges\Ratings\Task Order 46\zScratch\MLW\TO46_Template.xls'
+	wb_path = r'\\kcow00\Jobs3\50790\Bridges\Ratings\Task Order 46\Br 39.02\Rating Calculation\TO46_39.02.xls'
 	print_area = 'A1:N65'
 	
-	o, wb = open(wb_path)
+	wb = open(wb_path)
 	try:
-		print_multiple_ws(wb, wb_path, [1,4,5], print_area)
+		print_single_ws(wb, wb_path, 16, print_area)
 	except Exception, e:
 		exc_type, exc_value, exc_traceback = sys.exc_info()
 		traceback.print_exception(exc_type, exc_value, exc_traceback)
-	close(o, wb)
+	close(wb)
